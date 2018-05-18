bd6890c2eea982be54eedd2f10e23933ea96f1bb
Anton Iokov <it.iokov@gmail.com>
2018-05-18 14:08:50 +0200

    add all-ins to tooltip

--- wins-losses/js/tooltip.js
+++ wins-losses/js/tooltip.js
@@ -1,27 +1,32 @@
-function generateName (game) {
-    const typeLabel = game['Тип'] === 'Финал' ? 'Финал' : `${game['Тип']} игра`;
-    const seriesLabel = {
-        'Зима': 'года',
-        'Весна': 'весны',
-        'Лето': 'лета',
-        'Осень': 'осени',
-    }[game['Серия']];
+const label = {
+    'name': game => {
+        const typeLabel = game['Тип'] === 'Финал' ? 'Финал' : `${game['Тип']} игра`;
+        const seriesLabel = {
+            'Зима': 'года',
+            'Весна': 'весны',
+            'Лето': 'лета',
+            'Осень': 'осени',
+        }[game['Серия']];
 
-    return `${typeLabel} ${seriesLabel}, ${game['Дата'].getFullYear()}`;
-}
+        return `${typeLabel} ${seriesLabel}, ${game['Дата'].getFullYear()}`;
+    },
+
+    'team': game => game['Команда'],
+    'result': game => {
+        const allInLabel = game['РР'] ? ' (РР)' : '';
+        return `${game['Знатоки']}:${game['Телезрители']}${allInLabel}`;
+    },
+    'best-player': game => game['Лучший знаток']
+};
 
 
 function showTooltip(game, index) {
-    const tooltip = d3.select('#tooltip');
-
-
-    d3.select('#tooltip-name').text(generateName(game));
-    d3.select('#tooltip-team').text(game['Команда']);
-    d3.select('#tooltip-result').text(`${game['Знатоки']}:${game['Телезрители']}`);
-    d3.select('#tooltip-best-player').text(game['Лучший знаток']);
+    ['name', 'team', 'result', 'best-player'].forEach(part => d3.select(`#tooltip-${part}`).text(label[part](game)));
 
     const rect = d3.event.target.getBoundingClientRect();
     const margin = 4;
+
+    const tooltip = d3.select('#tooltip');
 
     tooltip
         .style('left',`${rect.right + margin}px`)
