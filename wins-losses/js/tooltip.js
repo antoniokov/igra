0f94f2801d0f96d5044fb2c8054e647cdfea9c4d
Anton Iokov <it.iokov@gmail.com>
2018-07-01 14:53:52 +0300

    add ugly fix for tooltip position

--- wins-losses/js/tooltip.js
+++ wins-losses/js/tooltip.js
@@ -23,14 +23,21 @@ const label = {
 function showTooltip(game, index) {
     ['name', 'team', 'result', 'best-player'].forEach(part => d3.select(`#tooltip-${part}`).text(label[part](game)));
 
-    const rect = d3.event.target.getBoundingClientRect();
+
     const margin = 4;
+    const width = 240+12+12;
+    const height = 100+12;
+
+    const rect = d3.event.target.getBoundingClientRect();
+
+    const left = rect.right > 550 ? rect.right - width - margin : rect.right + margin;
+    const bottom = rect.bottom > 300 ? rect.bottom - height - margin : rect.bottom + margin;
 
     const tooltip = d3.select('#tooltip');
 
     tooltip
-        .style('left',`${rect.right + margin}px`)
-        .style('top', `${rect.bottom + margin}px`);
+        .style('left',`${left}px`)
+        .style('top', `${bottom}px`);
 
     tooltip
         .style('visibility', 'visible');
