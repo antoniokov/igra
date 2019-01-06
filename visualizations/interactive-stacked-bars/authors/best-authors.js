32818780e764b27dc88d08b0e5240e81493c09d3
Anton Iokov <it.iokov@gmail.com>
2019-01-06 12:14:28 +0300

    exclude sponsors in best authors

--- visualizations/interactive-stacked-bars/authors/best-authors.js
+++ visualizations/interactive-stacked-bars/authors/best-authors.js
@@ -2,7 +2,7 @@ export default {
     id: 'best-authors',
     entity: 'Телезритель',
     dataSource: 'authors',
-    preFilter: (row) => row['Игр'] >= 5,
+    preFilter: (row) => row['Людей'] !== 0 && row['Игр'] >= 5,
     measures: ['Игр', 'Процент побед', 'Лучший телезритель', 'Процент лучших'],
     top: 24
 };
